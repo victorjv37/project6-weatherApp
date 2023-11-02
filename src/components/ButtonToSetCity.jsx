a22259d3fba8d70742bda3ab8d0d897985b6ddc2
Victor <victorjv37@gmail.com>
2023-11-02 16:08:01 +0100

    mas

--- src/components/ButtonToSetCity.jsx
+++ src/components/ButtonToSetCity.jsx
@@ -1,34 +1,64 @@
 import cityWeatherContext from "../context/CityWeatherContext";
 import { useContext } from "react";
 
-const ButtonToSetCity = ({setShowButtonToSetCity}) => {
+const ButtonToSetCity = ({ setShowButtonToSetCity }) => {
   const { setCityName, cityName } = useContext(cityWeatherContext);
 
   const handleCity = (ev) => {
-      setCityName(ev.target.value);
-      setShowButtonToSetCity(false)
-      console.log(cityName);
+    setCityName(ev.target.value);
+    setShowButtonToSetCity(false);
+    console.log(cityName);
   };
 
   return (
     <div className="buttonContainer">
-      <button className='cityButton' id="valencia" value="Valencia" onClick={handleCity}>
-        <p>Valencia</p>
+      <button
+        className="cityButton"
+        id="valencia"
+        value="Valencia"
+        onClick={handleCity}
+      >
+        <p className="p">Valencia</p>
       </button>
-      <button className='cityButton' id="madrid" value="Madrid" onClick={handleCity}>
-        <p>Madrid</p>
+      <button
+        className="cityButton"
+        id="madrid"
+        value="Madrid"
+        onClick={handleCity}
+      >
+        <p className="p">Madrid</p>
       </button>
-      <button className='cityButton' id="barcelona" value="Barcelona" onClick={handleCity}>
-      <p>Barcelona</p>
+      <button
+        className="cityButton"
+        id="barcelona"
+        value="Barcelona"
+        onClick={handleCity}
+      >
+        <p className="p">Barcelona</p>
       </button>
-      <button className='cityButton' id="sevilla" value="Sevilla" onClick={handleCity}>
-      <p>Sevilla</p>
+      <button
+        className="cityButton"
+        id="sevilla"
+        value="Sevilla"
+        onClick={handleCity}
+      >
+        <p className="p">Sevilla</p>
       </button>
-      <button className='cityButton' id="galicia" value="Galicia" onClick={handleCity}>
-        <p>Galicia</p>
+      <button
+        className="cityButton"
+        id="galicia"
+        value="Galicia"
+        onClick={handleCity}
+      >
+        <p className="p">Galicia</p>
       </button>
-      <button className='cityButton' id="toledo" value="Toledo" onClick={handleCity}>
-      <p>Toledo</p>
+      <button
+        className="cityButton"
+        id="toledo"
+        value="Toledo"
+        onClick={handleCity}
+      >
+        <p className="p">Toledo</p>
       </button>
     </div>
   );
